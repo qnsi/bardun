cdb7d0ef892df368f19cc74b2eedf3cc45b1ee7c
Artur Kęsik <arturkesik@gmail.com>
2022-02-16 10:02:29 +0100

    Add basic js from static files

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -12,7 +13,7 @@ type HomeData struct {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles("templates/home.gohtml")
+	tpl, err := template.ParseFiles("templates/home.html")
 	if err != nil {
 		panic(err)
 	}
@@ -22,11 +23,21 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("./static/home.js")
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "text/javascript")
+	w.Write(data)
+}
+
 func main() {
 	// db := utils.PrepareDB()
 	// contacts := models.GetContacts(db)
 	// fmt.Println(contacts[0].FirstName)
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/static/home.js", staticHandler)
 	fmt.Println("Starting server at :3000...")
 	http.ListenAndServe(":3000", nil)
 }
---
+++ static/home.js
@@ -0,0 +1,1 @@
+alert("Hello world! JS is working");
