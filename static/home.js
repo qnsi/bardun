f3feaa7191674a43d99b3e077769bc916974ff26
Artur Kęsik <arturkesik@gmail.com>
2022-02-16 12:02:58 +0100

    Add link overlay, to click between links

--- static/home.js
+++ static/home.js
@@ -1,7 +1,51 @@
 window.addEventListener("load", function () {
-  console.log(document.getElementById("editor"));
-  var cm = CodeMirror.fromTextArea(document.getElementById("editor"));
-  // var cm = CodeMirror.fromTextArea(document.body);
-  console.log(cm);
-  // cm.getDoc().setValue("Hello world!");
+  CodeMirror.defineMode("noteLink", function (config, parserConfig) {
+    var noteLinkOverlay = {
+      token: function (stream, state) {
+        var ch;
+        if (stream.match("[[")) {
+          while ((ch = stream.next()) != null)
+            if (ch == "]" && stream.next() == "]") {
+              stream.eat("]");
+              // Return style for lexed element
+              return "noteLink";
+            }
+        }
+        if (stream.match("[A")) {
+          while ((ch = stream.next()) != null)
+            if (ch == "A" && stream.next() == "]") {
+              stream.eat("]");
+              // Return style for lexed element
+              return "ankiLink";
+            }
+        }
+        while (stream.next() != null && !stream.match("[[", false)) {}
+        return null;
+      },
+    };
+    return CodeMirror.overlayMode(
+      CodeMirror.getMode(config, parserConfig.backdrop || "markdown"),
+      noteLinkOverlay
+    );
+  });
+
+  var cm = CodeMirror.fromTextArea(document.getElementById("editor"), {
+    lineNumbers: true,
+    lineWrapping: true,
+    theme: "default",
+    keyMap: "vim",
+    vieportMargin: 100000000,
+    mode: "noteLink",
+  });
+  cm.getDoc().setValue("Hello world!");
+
+  cm.getWrapperElement().addEventListener("click", (event) => {
+    var clickedDiv = event.target;
+    if (clickedDiv.classList[0] === "cm-noteLink") {
+      var linkNameDirty = clickedDiv.textContent;
+      var linkName = linkNameDirty.substr(2).slice(0, -2);
+      console.log("Loading file:...)");
+      console.log(linkName);
+    }
+  });
 });
